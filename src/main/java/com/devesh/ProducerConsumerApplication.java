b587e2a8bc7f31f8482003f7d320dfd0bd73d8b8
Devesh Kumar Patar <deveshpatar@gmail.com>
2020-02-28 13:02:33 +0530

    Initial Commit

---
+++ src/main/java/com/devesh/ProducerConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.devesh;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devesh.producer.Sender;
+
+@SpringBootApplication
+public class ProducerConsumerApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerConsumerApplication.class, args);
+    }
+
+    @Autowired
+    private Sender sender;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        String data = "Spring Kafka Producer and Consumer Example";
+        sender.send(data);
+    }
+
+}
